a9f24efc59204dbe1f2b7b2ded7273ed919f8058
Aurélien <aurelienarnaud921@gmail.com>
2025-04-24 13:13:13 +0800

    feature: ajout animation fakeLoader

--- src/compoments/pages/homePage/sections/Banner.tsx
+++ src/compoments/pages/homePage/sections/Banner.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 /** Affiche un questionaire de navigation avec un style de terminal
  * @description Utilise un tableaux de question et le useState pour gérer l'affichage de la question
@@ -9,7 +9,6 @@ import { useState } from 'react';
  */
 function Cmd(): React.JSX.Element {
   const [questionLength, setQuestionLength] = useState(1);
-
   const questionList = [
     { question: 'Vous souhaitez me contacter ?', direction: '#footer' },
     {
@@ -20,7 +19,7 @@ function Cmd(): React.JSX.Element {
   ];
 
   return (
-    <div className="font-code ">
+    <div className="font-code">
       {questionList.map((element, index) => (
         <div
           key={`question-${index}`}
@@ -45,6 +44,14 @@ function Cmd(): React.JSX.Element {
   );
 }
 
+function FakeLoader(): React.JSX.Element {
+  return (
+    <div className="flex justify-center">
+      <div className="bg-color-three h-1 fake-loader rounded-md"></div>
+    </div>
+  );
+}
+
 /** Section Banner
  * @description Affiche la section Banner à l'interieur de HomePage
  * @returns {JSX.Element} - Section Banner
@@ -53,6 +60,13 @@ function Cmd(): React.JSX.Element {
 function Banner(): React.JSX.Element {
   const text =
     'Passionné par le code. Mon parcours atypique m’a appris à apprendre vite, partout dans le monde. Aujourd’hui, je crée des interfaces claires et responsives avec React';
+
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1250);
+  }, []);
 
   return (
     <section id="banner" className="h-screen flex flex-col">
@@ -66,7 +80,7 @@ function Banner(): React.JSX.Element {
           Voir mes projets
         </Link>
       </div>
-      <Cmd />
+      {isLoading ? <FakeLoader /> : <Cmd />}
     </section>
   );
 }
