b2bb8f11247d998f3b6ff81d71e65485ba5d666e
Aurélien <aurelienarnaud921@gmail.com>
2025-04-25 16:35:44 +0800

    feature : sizing section

--- src/compoments/pages/homePage/sections/About.tsx
+++ src/compoments/pages/homePage/sections/About.tsx
@@ -1,31 +1,49 @@
 import photoProfile from '../../../../assets/photo-test.webp';
 
-function About() {
+function About(): React.JSX.Element {
+  const TextUn = (): React.JSX.Element => (
+    <p className="w-1/2 border-1 border-indigo-500">
+      Après 10 ans dans l’industrie en tant qu’opérateur et pilote de ligne
+      industriel, j’ai décidé de donner un nouveau souffle à mon parcours. En
+      quête de sens et de défis, j’ai entrepris un tour du monde de l’Asie
+      centrale à l’Amérique latine. Cette aventure m’a permis de développer mon
+      autonomie, mon adaptabilité et mon envie constante d’apprendre. C’est au
+      retour que j’ai choisi de me reconvertir dans le développement web, un
+      domaine qui allie logique technique et création.
+    </p>
+  );
+
+  const TextDeux = (): React.JSX.Element => (
+    <p className="border-1 border-indigo-500">
+      J’ai alors suivi une formation à distance tout en vivant à l’étranger, ce
+      qui m’a appris à travailler de manière disciplinée et autonome dans tous
+      les contextes. Aujourd’hui, je conçois des sites et applications web
+      modernes, performants et centrés utilisateur avec des technologies comme
+      HTML, CSS, JavaScript, React et Node.js. Curieux, adaptable et rigoureux,
+      je suis à la recherche d’un poste ou de missions freelance pour continuer
+      à évoluer et contribuer à des projets concrets.
+    </p>
+  );
+
   return (
-    <section id="about" className="flex mb-6 border-1 border-indigo-500">
-      <img
-        src={photoProfile}
-        alt="Photo de aurélien Arnaud"
-        className="w-1/4 h-full"
-      ></img>
-      <p>
-        Après 10 ans dans l’industrie en tant qu’opérateur et pilote de ligne
-        industriel, j’ai décidé de donner un nouveau souffle à mon parcours. En
-        quête de sens et de défis, j’ai entrepris un tour du monde à travers
-        l’Asie, l’Amérique latine et l’Asie centrale. Cette aventure m’a permis
-        de développer mon autonomie, mon adaptabilité et mon envie constante
-        d’apprendre. C’est au retour que j’ai choisi de me reconvertir dans le
-        développement web, un domaine qui allie logique technique et création.
-        J’ai suivi une formation à distance tout en vivant à l’étranger
-        (Thaïlande, Vietnam, Taïwan), ce qui m’a appris à travailler de manière
-        disciplinée et autonome dans tous les contextes. Aujourd’hui, je conçois
-        des sites et applications web modernes, performants et centrés
-        utilisateur avec des technologies comme HTML, CSS, JavaScript, React et
-        Node.js. Curieux, adaptable et rigoureux, je suis à la recherche d’un
-        poste ou de missions freelance pour continuer à évoluer et contribuer à
-        des projets concrets.
-      </p>
+    <section id="about" className="">
+      <div className="flex justify-between">
+        <ProfilCard />
+        <TextUn />
+      </div>
     </section>
   );
 }
 export default About;
+
+function ProfilCard(): React.JSX.Element {
+  return (
+    <article className="w-1/3 border-1 border-indigo-500">
+      <img src={photoProfile} alt="Photo de aurélien Arnaud" className=""></img>
+      <div className="">
+        <span>Développeur web</span>
+        <button className="btn">Télécharger mon CV</button>
+      </div>
+    </article>
+  );
+}
