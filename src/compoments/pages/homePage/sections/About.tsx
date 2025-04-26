123833278b8081dd3e314fb0be980548ecb7596f
Aurélien <aurelienarnaud921@gmail.com>
2025-04-26 16:30:02 +0800

    feature: ajout du texte dans la section about

--- src/compoments/pages/homePage/sections/About.tsx
+++ src/compoments/pages/homePage/sections/About.tsx
@@ -1,49 +1,52 @@
-import photoProfile from '../../../../assets/photo-test.webp';
-
 function About(): React.JSX.Element {
-  const TextUn = (): React.JSX.Element => (
-    <p className="w-1/2 border-1 border-indigo-500">
-      Après 10 ans dans l’industrie en tant qu’opérateur et pilote de ligne
-      industriel, j’ai décidé de donner un nouveau souffle à mon parcours. En
-      quête de sens et de défis, j’ai entrepris un tour du monde de l’Asie
-      centrale à l’Amérique latine. Cette aventure m’a permis de développer mon
-      autonomie, mon adaptabilité et mon envie constante d’apprendre. C’est au
-      retour que j’ai choisi de me reconvertir dans le développement web, un
-      domaine qui allie logique technique et création.
-    </p>
-  );
-
-  const TextDeux = (): React.JSX.Element => (
-    <p className="border-1 border-indigo-500">
-      J’ai alors suivi une formation à distance tout en vivant à l’étranger, ce
-      qui m’a appris à travailler de manière disciplinée et autonome dans tous
-      les contextes. Aujourd’hui, je conçois des sites et applications web
-      modernes, performants et centrés utilisateur avec des technologies comme
-      HTML, CSS, JavaScript, React et Node.js. Curieux, adaptable et rigoureux,
-      je suis à la recherche d’un poste ou de missions freelance pour continuer
-      à évoluer et contribuer à des projets concrets.
-    </p>
-  );
+  const santanceList = [
+    {
+      text: 'Après 10 ans dans l’industrie en tant qu’opérateur et pilote de ligne industriel, j’ai décidé de donner un nouveau souffle à mon parcours.',
+      classStyle: '',
+      key: 'santance0',
+    },
+    {
+      text: 'En quête de sens et de défis, j’ai entrepris un tour du monde. Cette aventure m’a permis de développer mon autonomie, mon adaptabilité et mon envie constante d’apprendre.',
+      classStyle: 'self-end',
+      key: 'santance1',
+    },
+    {
+      text: 'C’est au retour que j’ai choisi de me reconvertir dans le développement web, un domaine qui allie logique technique et création.',
+      classStyle: '',
+      key: 'santance2',
+    },
+    {
+      text: 'J’ai alors suivi une formation à distance tout en vivant à l’étranger, ce qui m’a appris à travailler de manière disciplinée et autonome dans tous les contextes.',
+      classStyle: 'self-end',
+      key: 'santance3',
+    },
+    {
+      text: 'Aujourd’hui, je conçois des sites et applications web modernes, performants et centrés utilisateur avec des technologies comme HTML, CSS, JavaScript, React et Node.js.',
+      classStyle: '',
+      key: 'santance4',
+    },
+    {
+      text: 'Curieux, adaptable et rigoureux, je suis à la recherche d’un poste ou demissions freelance pour continuer à évoluer et contribuer à des projets concrets.',
+      classStyle: 'self-end',
+      key: 'santance5',
+    },
+  ];
 
   return (
-    <section id="about" className="">
-      <div className="flex justify-between">
-        <ProfilCard />
-        <TextUn />
+    <section id="about" className="flex flex-col gap-8 md:gap-16 ">
+      <h2 className="">Trouver un Titrre</h2>
+
+      <div className="h-full flex flex-col gap-4 md:gap-0 md:justify-between pb-4">
+        {santanceList.map(santance => (
+          <span
+            key={santance.key}
+            className={`${santance.classStyle} w-2/3 md:text-center`}
+          >
+            {santance.text}
+          </span>
+        ))}
       </div>
     </section>
   );
 }
 export default About;
-
-function ProfilCard(): React.JSX.Element {
-  return (
-    <article className="w-1/3 border-1 border-indigo-500">
-      <img src={photoProfile} alt="Photo de aurélien Arnaud" className=""></img>
-      <div className="">
-        <span>Développeur web</span>
-        <button className="btn">Télécharger mon CV</button>
-      </div>
-    </article>
-  );
-}
