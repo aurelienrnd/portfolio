7e80a45a0bad327c363c74b556c282903f54177d
Aurélien <aurelienarnaud921@gmail.com>
2025-04-28 14:28:00 +0800

    feature: tableaux de competance

--- src/compoments/pages/homePage/sections/Skilles.tsx
+++ src/compoments/pages/homePage/sections/Skilles.tsx
@@ -28,10 +28,10 @@ function Skilles() {
   return (
     <section
       id="skilles"
-      className="flex flex-col gap-8 md:gap-16 border-1 border-indigo-500"
+      className="flex flex-col gap-8 md:gap-8 border-1 border-indigo-500"
     >
       <h2>Mes compétence de developpeur</h2>
-      <div className="flex flex-wrap gap-16">
+      <div className="flex flex-wrap justify-center gap-16 items-center ">
         {catSkillesList.map((cat, index) => (
           <Category key={`${cat.id}-${index}`} cat={cat} />
         ))}
@@ -43,9 +43,9 @@ export default Skilles;
 
 function Category({ cat }: CatProp) {
   return (
-    <div className="flex flex-col gap-4">
-      <h3 className="text-3xl">{cat.name}</h3>
-      <div>
+    <div className=" flex flex-col w-2/5 gap-4 skillCard">
+      <h3 className="text-2xl">{cat.name}</h3>
+      <div className="flex flex-col gap-4">
         {cat.skilles.map((skill, index) => (
           <Skill key={`${cat.id}-${skill.id}-${index}`} skill={skill} />
         ))}
@@ -56,8 +56,8 @@ function Category({ cat }: CatProp) {
 
 function Skill({ skill }: SkillProp) {
   return (
-    <div className="flex flex-col gap-2">
-      <h4 className="text-2xl">{skill.name}</h4>
+    <div className="flex flex-col gap-2 items-start text-start">
+      <h4 className="">{skill.name}</h4>
       <div>
         {skill.commentaire &&
           skill.commentaire.map((com, index) => (
@@ -69,5 +69,9 @@ function Skill({ skill }: SkillProp) {
 }
 
 function Commentaire({ com }: comProp) {
-  return <p>{com}</p>;
+  return (
+    <ul>
+      <li>{com}</li>
+    </ul>
+  );
 }
