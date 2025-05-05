f6cfda586abadf2938db1de23f4d243b7ab15ba7
Aurélien <aurelienarnaud921@gmail.com>
2025-05-05 10:44:43 +0800

    fix section skilles : modiffication style h-min, h-max and mediaQuery

--- src/compoments/pages/homePage/sections/Skilles.tsx
+++ src/compoments/pages/homePage/sections/Skilles.tsx
@@ -70,17 +70,17 @@ function Skilles(): React.JSX.Element {
     <section id="skilles">
       <h2>Mes compétence en developpement web</h2>
 
-      <div className="flex flex-col gap-8 md:h-full md:justify-between">
-        <div className="flex flex-col justify-around gap-4 md:h-3/4">
+      <div className="flex flex-col gap-8 h-full md:justify-between">
+        <div className=" md:flex md:flex-col md:justify-between md:h-full">
           {skilles.map((skill, index) => (
             <Skill key={`${skill.technologie}-${index}`} skill={skill} />
           ))}
         </div>
 
-        <div className="flex flex-col gap-4 md:h-1/4 md:justify-center">
-          <h3 className="">Compétences transverses & soft skills</h3>
+        <div className="flex flex-col gap-4">
+          <h3>Compétences transverses & soft skills</h3>
 
-          <div className="flex flex-wrap gap-2">
+          <div className="flex flex-wrap gap-2 justify-center md:justify-start">
             {softSkilles.map((skilles, index) => (
               <span
                 key={`${skilles.name}-${index}`}
