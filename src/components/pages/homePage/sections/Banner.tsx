d723755f35823a230d69469161e8cac5f379d54b
Aurélien <aurelienarnaud921@gmail.com>
2025-05-29 16:43:52 +0800

    feature Banner.tsx style.css: ajout annimation

--- src/components/pages/homePage/sections/Banner.tsx
+++ src/components/pages/homePage/sections/Banner.tsx
@@ -31,16 +31,16 @@ function Banner(): React.JSX.Element {
   return (
     <section id="banner">
       <div className="h-2/3 md:h-1/2 text-center md:text-start flex flex-col justify-between">
-        <h1>Aurélien Arnaud</h1>
+        <h1 className="slide-in-left">Aurélien Arnaud</h1>
 
         <div className=" h-1/3 flex flex-col md:flex-row items-center md:items-start justify-between gap-4">
-          <h2>Développeur d'application web</h2>
-          <Link to="project" className="btn md:mr-8">
+          <h2 className="slide-in-left">Développeur d'application web</h2>
+          <Link to="project" className="btn md:mr-16 slide-in-right">
             Voir mes projets
           </Link>
         </div>
 
-        <p className="md:w-1/2">{text}</p>
+        <p className="md:w-1/2 slide-in-left">{text}</p>
       </div>
 
       {isLoading ? <FakeLoader /> : <Cmd />}
