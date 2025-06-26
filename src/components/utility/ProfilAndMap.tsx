091fe24d4e30143a83ca0125434beb9752277180
Aurélien <aurelienarnaud921@gmail.com>
2025-06-26 12:05:03 +0200

    fix ProfilAndMap.tsx :

--- src/components/utility/ProfilAndMap.tsx
+++ src/components/utility/ProfilAndMap.tsx
@@ -19,7 +19,7 @@ function ProfilAndMap() {
       <button
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className={`absolute z-2 flex h-full flex-col justify-between items-start cursor-pointer bg-color-two/30 py-4 px-4 ${isHovered ? 'w-auto' : 'w-12'}`}
+        className={`absolute z-2 flex h-full flex-col justify-between items-start cursor-pointer bg-color-two/30 py-4 px-4 transition-all duration-300 ease-in-out ${isHovered ? 'w-40' : 'w-12'}`}
         onClick={handleBouton}
         aria-label={
           visibleMap
