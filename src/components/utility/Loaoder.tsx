e6c3dd08ff0907450319de7243f2ff3d1950b544
Aurélien <aurelienarnaud921@gmail.com>
2025-06-26 15:34:05 +0200

    modif Banner.tsx : Suppression du composent FakeLoader et creation d'un composent Loader dans un fichier Loader.tsx pour le remplacer

---
+++ src/components/utility/Loaoder.tsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="h-1/3 flex w-1/2  items-center justify-center">
+      <div className="bg-color-three h-1 fake-loader rounded-md"></div>
+    </div>
+  );
+}
+
+export default Loader;
