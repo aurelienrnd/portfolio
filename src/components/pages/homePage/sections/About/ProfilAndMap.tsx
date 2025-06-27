839fcfb38fc931c07630745ee3bd3b93f91c88ca
Aurélien <aurelienarnaud921@gmail.com>
2025-06-27 10:27:38 +0200

    Modif ProfilAndMap : création du composant LoadingMap pour gérer le chargement asynchrone de la carte dans ProfilAndMap

--- src/components/pages/homePage/sections/About/ProfilAndMap.tsx
+++ src/components/pages/homePage/sections/About/ProfilAndMap.tsx
@@ -1,18 +1,14 @@
 // Hooks
-import { useState, lazy, Suspense } from 'react';
-
+import { useState } from 'react';
 //Compoments
 import MapButton from './MapButton.tsx';
-import Loader from '../../../../utility/Loaoder.tsx';
-const Map = lazy(() => import('./Map.tsx'));
+import LoadingMap from './LoadingMap.tsx';
 
-/** Lorsqu'on clique sur le bouton, il affiche soit une photo de profil, soit une carte du tour du monde.
- * @description - Utilise useState pour déterminer si la carte doit être affichée. Si oui, la carte est chargée dynamiquement avec lazy et Suspense, accompagnée d’un loader.
+/** Lorsqu'on clique sur le bouton, affiche soit une photo de profil, soit une carte du tour du monde.
+ * @description - Utilise useState pour déterminer si la carte doit être affichée. Si oui, la carte est chargée dynamiquement.
  * Sinon, une photo de profil est affichée à la place.
  * @component MapButton - Affiche le bouton permettant de basculer entre la carte et la photo de profil.
- * @component Map - Affiche la carte du tour du monde.
- * @component Loader - Affiche un loader pendant le chargement de la carte.
- * @returns {JSX.Element} - Un bouton, une image ou une carte interactive selon l’état.
+ * @returns {JSX.Element} - Un bouton et une image ou une carte interactive selon l’état.
  */
 function ProfilAndMap(): React.JSX.Element {
   // Gère l’état d’affichage entre la carte et la photo de profil
@@ -23,19 +19,7 @@ function ProfilAndMap(): React.JSX.Element {
       <MapButton visibleMap={visibleMap} setVisibleMap={setVisibleMap} />
 
       {visibleMap ? (
-        <Suspense
-          fallback={
-            <div className="flex flex-col items-center justify-center md:font-code h-60 md:h-full w-full object-cover">
-              <div className="flex gap-4 items-center">
-                <span>Chargement de la carte...</span>
-              </div>
-
-              <Loader />
-            </div>
-          }
-        >
-          <Map />
-        </Suspense>
+        <LoadingMap />
       ) : (
         <img
           src="/images/profil.webp"
