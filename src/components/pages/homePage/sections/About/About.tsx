30c169935e02f9dfa8345eff14862a6efb6a6a8f
Aurélien <aurelienarnaud921@gmail.com>
2025-06-27 10:55:15 +0200

    modif About.tsx : affichage du texte de la section About via appel API à about.json et mapping des données

--- src/components/pages/homePage/sections/About/About.tsx
+++ src/components/pages/homePage/sections/About/About.tsx
@@ -1,40 +1,39 @@
-//Compoments
-import ProfilAndMap from './ProfilAndMap';
+// Hooks
+import { useEffect, useState } from 'react';
+//Components
+import ProfilAndMap from './ProfilAndMap.tsx';
+//types
+interface TextType {
+  key: string;
+  paragraphe: string;
+}
 
-/** Section About
- * @description - Affiche la section About.
- * @component ProfilAndMap - Affiche la photo de profil ou une carte du tour du monde.
+/** Section À propos
+ * @description - Effectue une requête pour récupérer les données textuelles, puis utilise `.map` pour afficher chaque paragraphe,
+ * accompagnés d’une photo de profil ou d’une carte du tour du monde.
+ * @component ProfilAndMap - Affiche soit la photo de profil, soit la carte du tour du monde.
  * @returns {JSX.Element} - La section À propos du site.
  */
 function About(): React.JSX.Element {
+  const [text, setText] = useState<TextType[]>([]);
+
+  useEffect(() => {
+    fetch('/datas/about.json')
+      .then(res => res.json())
+      .then(data => setText(data))
+      .catch(err => console.error('Erreur chargement hardskills:', err));
+  }, []);
+
   return (
     <section id="about">
       <h2>Mon parcours de développeur</h2>
       <div className="flex flex-col-reverse md:flex-row items-center gap-8 md:justify-between md:h-full">
         <div className="flex flex-col gap-4 md:justify-between md:w-2/3 md:h-full md:max-h-150">
-          <p>
-            Après 10 ans dans l’industrie en tant qu’opérateur et pilote de
-            ligne, j’ai décidé de donner un nouveau souffle à mon parcours.
-          </p>
-          <p>
-            En quête de sens et de défis, j’ai entrepris un tour du monde à
-            travers l’Asie, l’Amérique latine et l’Asie centrale. Cette aventure
-            m’a permis de développer mon autonomie, mon adaptabilité et mon
-            envie constante d’apprendre. C’est au retour que j’ai choisi de me
-            reconvertir dans le développement web, un domaine qui allie logique
-            technique et création. J’ai suivi une formation à distance tout en
-            vivant à l’étranger (Thaïlande, Vietnam, Taïwan), ce qui m’a appris
-            à travailler de manière disciplinée et autonome dans tous les
-            contextes.
-          </p>
-          <p>
-            Aujourd’hui, je conçois des sites et applications web modernes,
-            performants et centrés utilisateur avec des technologies comme HTML,
-            CSS, JavaScript, React et Node.js. Curieux, adaptable et rigoureux,
-            je suis à la recherche d’un poste ou de missions freelance pour
-            continuer à évoluer et contribuer à des projets concrets.
-          </p>
+          {text.map(item => (
+            <p key={item.key}>{item.paragraphe}</p>
+          ))}
         </div>
+
         <ProfilAndMap />
       </div>
     </section>
