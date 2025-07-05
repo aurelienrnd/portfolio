4360dc0f73056a17950213c3b61d57e624912ed0
Aurélien <aurelienarnaud921@gmail.com>
2025-07-05 21:25:49 +0200

    feature CarouselProject.tsx : importation de useEmblaCarousel

--- src/components/pages/projects/sections/my-project/CarouselProject.tsx
+++ src/components/pages/projects/sections/my-project/CarouselProject.tsx
@@ -1,11 +1,21 @@
+import useEmblaCarousel from 'embla-carousel-react';
 import type { ProjectsType } from './MyProject.tsx';
 interface Prop {
   projects: ProjectsType[];
 }
 
 function CarouselProject({ projects }: Prop): React.JSX.Element {
+  const [emblaRef] = useEmblaCarousel();
   console.log(projects);
-  return <div></div>;
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        <div className="embla__slide">Slide 1</div>
+        <div className="embla__slide">Slide 2</div>
+        <div className="embla__slide">Slide 3</div>
+      </div>
+    </div>
+  );
 }
 
 export default CarouselProject;
