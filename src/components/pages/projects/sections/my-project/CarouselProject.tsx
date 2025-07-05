6e378bc6bce8e925b654a1b0ff3bbd2299f2e5ea
Aurélien <aurelienarnaud921@gmail.com>
2025-07-05 21:58:12 +0200

    feature CarouselProject.tsx : creation du carousel

--- src/components/pages/projects/sections/my-project/CarouselProject.tsx
+++ src/components/pages/projects/sections/my-project/CarouselProject.tsx
@@ -1,18 +1,28 @@
 import useEmblaCarousel from 'embla-carousel-react';
 import type { ProjectsType } from './MyProject.tsx';
+
 interface Prop {
   projects: ProjectsType[];
 }
 
 function CarouselProject({ projects }: Prop): React.JSX.Element {
   const [emblaRef] = useEmblaCarousel();
-  console.log(projects);
+
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
+    <div className="w-full overflow-hidden" ref={emblaRef}>
+      <div className="flex">
+        {projects.map(project => (
+          <div
+            key={`project-${project.id}`}
+            className="md:flex-[0_0_40%] flex-[0_0_80%] min-w-0 px-4"
+          >
+            <img
+              src={project.image}
+              alt={project.title}
+              className="w-full h-60 object-cover rounded-xl shadow-md"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
