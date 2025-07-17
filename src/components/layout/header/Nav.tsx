25054085c85328f07bf50e47e342f96781478f69
Aurélien <aurelienarnaud921@gmail.com>
2025-07-17 10:30:39 +0200

    feature header/ : ajout de typage Ts

--- src/components/layout/header/Nav.tsx
+++ src/components/layout/header/Nav.tsx
@@ -5,23 +5,23 @@ import { Link } from 'react-router';
 import ReactModal from 'react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-// Types Tsx
-import { NavList } from './Header.tsx';
-
-type NavProps = {
-  navList: NavList[];
+// Types
+import { NavType } from './Header.tsx';
+// Typage TypeScript
+type PropsType = {
+  navList: NavType[];
 };
 
 /** Menu de navigation pour les écrans larges
  * @description - Utilise `.map` sur le tableau navList pour créer une balise Link pour chaque élément.
- * @param {NavProps} navList - Tableau d'objets contenant les destinations et les labels de navigation.
+ * @param {NavType[]} navList - Tableau d'objets contenant les destinations et les labels de navigation.
  * @returns {JSX.Element} - Le menu de navigation pour les écrans larges.
  */
-export function NavDesktop({ navList }: NavProps): React.JSX.Element {
+export function NavDesktop({ navList }: PropsType): React.JSX.Element {
   return (
     <nav className="hidden md:flex items-center h-full">
       <ul className="flex gap-8 items-center h-full">
-        {navList.map(nav => (
+        {navList.map((nav: NavType) => (
           <li key={`nav-${nav.label}`} className="flex items-center h-full">
             <Link
               to={nav.destination}
@@ -39,13 +39,13 @@ export function NavDesktop({ navList }: NavProps): React.JSX.Element {
 /** Menu de navigation pour les petits écrans
  * @description - Utilise ReactModal pour créer un menu de navigation qui s’ouvre et se ferme au clic sur le bouton hamburger.
  * Utilise `.map` sur le tableau navList pour générer une balise Link pour chaque élément.
- * @param {NavProps} navList - Tableau d'objets contenant les destinations et les labels de navigation.
+ * @param {NavType[]} navList - Tableau d'objets contenant les destinations et les labels de navigation.
  * @returns {JSX.Element} - Le menu de navigation pour les petits écrans.
  */
-export function NavMobile({ navList }: NavProps): React.JSX.Element {
+export function NavMobile({ navList }: PropsType): React.JSX.Element {
   // Gestion de l’ouverture ou de la fermeture de la modale
-  const [isOpen, setIsOpen] = useState(false);
-  const handleMenuToggle = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleMenuToggle = (): void => {
     setIsOpen(!isOpen);
   };
 
@@ -61,7 +61,7 @@ export function NavMobile({ navList }: NavProps): React.JSX.Element {
         className="fixed top-16 right-0 w-2/3 bg-color-four z-9999 flex justify-center items-center rounded-bl-md openModal"
       >
         <nav className="flex flex-col items-start gap-4 my-6 w-2/3">
-          {navList.map(nav => (
+          {navList.map((nav: NavType) => (
             <Link
               to={nav.destination}
               key={nav.label}
